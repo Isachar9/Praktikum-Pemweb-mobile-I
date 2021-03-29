1a90efc29f195cb85c383b672d36827112e09f23
michaelisacharrr <81123991+Isachar9@users.noreply.github.com>
2021-03-29 08:57:35 +0700

    Create input_nama.php

---
+++ Laporan/MODUL 1/input_nama.php
@@ -0,0 +1,12 @@
+<?php
+	$keluarga = [
+		'nama_anggota'=>[
+			'Nama Ayah'=>'Tanggarson',
+			'Nama Ibu'=>'Ery Friani',
+			'Nama anak pertama'=>'Agnesia Gloria',
+			'Nama anak kedua'=>'Michael Isachar'
+		]
+        ];
+ 
+	echo $keluarga['nama_anggota']['Nama anak kedua']; 
+?>
