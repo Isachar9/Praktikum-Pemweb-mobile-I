5990759183068dfa4c2680468309c8e2c6126d12
GodRaiseMe <81123991+Isachar9@users.noreply.github.com>
2021-05-13 00:51:23 +0700

    Add files via upload

---
+++ Laporan/MODUL 5/modul5_193030503043/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const HelloWorldApp = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+      }}>
+       <Text>Praktikum Modul 5</Text>
+       <Text>Michael Isachar</Text>
+        <Text>(193030503043)</Text>
+    </View>
+  )
+}
+
+export default HelloWorldApp;
